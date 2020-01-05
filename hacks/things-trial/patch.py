0a5280ec006e2df5606a3f0d5c9e5e253707848c
gebeto <slavik.nychkalo@gmail.com>
2020-01-06 01:08:50 +0200

    improve things readme and remove license

--- hacks/things-trial/patch.py
+++
@@ -1,9 +0,0 @@
-import lief
-
-print("Parsing...")
-target = lief.parse("/Users/gebeto/Desktop/Github/thingstrial/target")
-print("Parsed...")
-target.add_library("/Users/gebeto/Desktop/Github/thingstrial/libthingstrial.dylib")
-print("Library added...")
-target.write("/Users/gebeto/Desktop/Github/thingstrial/target.new")
-print("DONE!")
