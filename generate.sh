3c5204a1ce4b95ae7dc10cfdf3fe4d14b8981040
gebeto <slavik.nychkalo@gmail.com>
2020-01-16 15:36:56 +0200

    Add apps hacks dock links generators

---
+++ generate.sh
@@ -0,0 +1,13 @@
+HACK_PATH=${1:-.}
+
+source "${HACK_PATH}/config"
+
+ROOT="${HACK_PATH}/"
+APP_ROOT="${ROOT}/${APP_NAME}.app"
+
+mkdir "${APP_ROOT}"
+
+cp "${HACK_PATH}/run.sh" "${APP_ROOT}/${APP_NAME}"
+cp "${HACK_PATH}/libpatch.dylib" "${APP_ROOT}/libpatch.dylib"
+
+cp "${ICON_PATH}" "${ROOT}/Icon.icns"
